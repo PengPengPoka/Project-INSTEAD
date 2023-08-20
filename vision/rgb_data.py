77f5723fd522e58f84fe488afc2d83e2a50bca0f
PengPengPoka <hafizhfanani03@gmail.com>
2023-08-20 13:45:28 +0700

    nambah dikit

--- vision/rgb_data.py
+++ vision/rgb_data.py
@@ -5,21 +5,18 @@ import matplotlib.pyplot as plt
 image_path = 'utils/chai_tea.jpg'
 image = cv.imread(image_path)
 
-cv.imshow("tea",image)
-
-crop = image[223:208, 365:354]
-# cv.imshow("crop",crop)
-print(crop)
+cv.imshow("image",image)
+crop = image[100:400,50:600]
+cv.imshow("crop",crop)
 
 key = cv.waitKey()
-
 if key == 27:
     exit()
 
-# color = ['b','g','r']
-# for channel, col in enumerate(color):
-#     histogram = cv.calcHist([image],[channel],mask=None,histSize=[256],ranges=[0,256])
-#     plt.plot(histogram,color = col)
-#     plt.xlim(0,256)
-# plt.title("Histogram of RGB values")
-# plt.show()
+color = ['b','g','r']
+for channel, col in enumerate(color):
+    histogram = cv.calcHist([crop],[channel],mask=None,histSize=[256],ranges=[0,256])
+    plt.plot(histogram,color = col)
+    plt.xlim(0,256)
+plt.title("Graph of RGB values")
+plt.show()
