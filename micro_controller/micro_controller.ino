6af07d7d3b3b3146afd5f468546065b944e274f8
Satria Pamungkas <satriap2101@gmail.com>
2023-08-27 17:49:17 +0700

    increase array size

--- micro_controller/micro_controller.ino
+++ micro_controller/micro_controller.ino
@@ -1,5 +1,3 @@
-HardwareSerial Serial2(PA10, PA9);
-
 #define SENSOR1 PC0
 #define SENSOR2 PC1
 #define SENSOR3 PC4
@@ -8,6 +6,8 @@ HardwareSerial Serial2(PA10, PA9);
 #define SENSOR6 PA5
 #define SENSOR7 PA6
 #define SENSOR8 PA7
+
+HardwareSerial Serial2(PA10, PA9);
 
 void setup() {
   Serial2.begin(115200);
@@ -26,32 +26,29 @@ void loop() {
   if (Serial2.available()) {
     String received_data = Serial2.readStringUntil('\n');
 
-    int received_values[2];
+    int received_values[9999];
     int value_index = 0;
     char separator = '#';
     int start = 0;
 
-    received_data.trim();  // Hapus \n di akhir
-    // Serial2.println(received_data);
+    received_data.trim();  // delete \n
 
+    // convert string to int
     for (int i = 0; i < received_data.length(); i++) {
       if (received_data[i] == separator || received_data[i] == '\n') {
-        received_values[value_index] = received_data.substring(start, i).toInt();  // Mengonversi string menjadi integer
+        received_values[value_index] = received_data.substring(start, i).toInt();  //split then convert to int
         value_index++;
         start = i + 1;
       }
     }
 
+    // convert last value to int
     if (value_index > 0) {
       received_values[value_index] = received_data.substring(start).toInt();
     }
 
-    // if (value_index >= 2) {
     int delay_ms = received_values[0];
-    // Serial2.println(delay_ms);
     int amount = received_values[1];
-    // Serial2.println(amount);
-    // }
 
     for (int i = 0; i < amount; i++) {
       Serial2.print(analogRead(SENSOR1));
