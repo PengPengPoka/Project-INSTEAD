68e9e2b9270849ce012d8eec4edbe39da1bef815
Satria Pamungkas <satriap2101@gmail.com>
2023-08-27 14:12:34 +0700

    parse, save to csv

--- micro_controller/micro_controller.ino
+++ micro_controller/micro_controller.ino
@@ -1,36 +1,76 @@
 HardwareSerial Serial2(PA10, PA9);
 
-#define POT1 PC0
-#define POT2 PC1
-#define POT3 PC4
-#define POT4 PC5
+#define SENSOR1 PC0
+#define SENSOR2 PC1
+#define SENSOR3 PC4
+#define SENSOR4 PC5
+#define SENSOR5 PA4
+#define SENSOR6 PA5
+#define SENSOR7 PA6
+#define SENSOR8 PA7
 
 void setup() {
   Serial2.begin(115200);
   analogReadResolution(16);
-  // pinMode(PA2, OUTPUT);
-  pinMode(POT1, INPUT);
-  pinMode(POT2, INPUT);
-  pinMode(POT3, INPUT);
-  pinMode(POT4, INPUT);
+  pinMode(SENSOR1, INPUT);
+  pinMode(SENSOR2, INPUT);
+  pinMode(SENSOR3, INPUT);
+  pinMode(SENSOR4, INPUT);
+  pinMode(SENSOR5, INPUT);
+  pinMode(SENSOR6, INPUT);
+  pinMode(SENSOR7, INPUT);
+  pinMode(SENSOR8, INPUT);
 }
 
 void loop() {
-  // digitalWrite(PA2, HIGH);
-  
-  // Serial2.println("POT 1: ");
-  // delay(500);
-  // digitalWrite(PA2, LOW);
-  Serial2.print(analogRead(POT1));
-  Serial2.print("#");
-  // Serial2.println("POT 2: ");
-  Serial2.print(analogRead(POT2));
-  Serial2.print("#");
-  Serial2.print(analogRead(POT3));
-  Serial2.print("#");
-  Serial2.print(analogRead(POT4));
-  // Serial2.print("#");
-  Serial2.print("\n");
-  
-  delay(100);
+  if (Serial2.available()) {
+    String received_data = Serial2.readStringUntil('\n');
+
+    int received_values[2];
+    int value_index = 0;
+    char separator = '#';
+    int start = 0;
+
+    received_data.trim();  // Hapus \n di akhir
+    // Serial2.println(received_data);
+
+    for (int i = 0; i < received_data.length(); i++) {
+      if (received_data[i] == separator || received_data[i] == '\n') {
+        received_values[value_index] = received_data.substring(start, i).toInt();  // Mengonversi string menjadi integer
+        value_index++;
+        start = i + 1;
+      }
+    }
+
+    if (value_index > 0) {
+      received_values[value_index] = received_data.substring(start).toInt();
+    }
+
+    // if (value_index >= 2) {
+    int delay_ms = received_values[0];
+    // Serial2.println(delay_ms);
+    int amount = received_values[1];
+    // Serial2.println(amount);
+    // }
+
+    for (int i = 0; i < amount; i++) {
+      Serial2.print(analogRead(SENSOR1));
+      Serial2.print("#");
+      Serial2.print(analogRead(SENSOR2));
+      Serial2.print("#");
+      Serial2.print(analogRead(SENSOR3));
+      Serial2.print("#");
+      Serial2.print(analogRead(SENSOR4));
+      Serial2.print("#");
+      Serial2.print(analogRead(SENSOR5));
+      Serial2.print("#");
+      Serial2.print(analogRead(SENSOR6));
+      Serial2.print("#");
+      Serial2.print(analogRead(SENSOR7));
+      Serial2.print("#");
+      Serial2.print(analogRead(SENSOR8));
+      Serial2.print("\n");
+      delay(delay_ms);
+    }
+  }
 }
--- micro_controller/serial_data.py
+++ micro_controller/serial_data.py
@@ -1,11 +1,25 @@
 import serial
- 
-ser = serial.Serial('COM7', baudrate = 115200, timeout=1)
- 
+import csv
+
+ser = serial.Serial('COM7', baudrate=115200)  # usb port settings
+csv_filename = "sensor_data.csv"
+
+
+def request_data(delay, amount):
+    data_to_send = f"{delay}#{amount}\n"
+    ser.write(data_to_send.encode())
+    with open(csv_filename, mode='a', newline='') as csv_file:
+        for i in range(int(amount)):
+            serial_data = ser.readline().decode('ascii')    # read serial data from usb
+            split_values = serial_data.split("#")   # split values by #
+            int_values = [int(value) for value in split_values]
+            print(int_values)  # Output: [123, 456, 789]
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(int_values)  # append data to csv file
+
+
 while 1:
-    arduinoData = ser.readline().decode('ascii')
-    split_values = arduinoData.split("#")
-    # int_values = list(map(int, split_values))
-    int_values = [int(value) for value in split_values]
-    print(int_values)  # Output: [123, 456, 789]
-    # print(arduinoData)
+    delay = input("Delay (ms): ")  # input delay from user
+    amount = input("How many data? ")  # input amount from user
+    # print(delay+amount)
+    request_data(delay, amount)
