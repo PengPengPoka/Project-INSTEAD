c81288b1cf361a4715297c3ced8db077c0d38ec7
Satria Pamungkas <satriap2101@gmail.com>
2023-09-20 17:52:01 +0700

    multiprocessing

--- micro_controller/micro_controller.ino
+++ micro_controller/micro_controller.ino
@@ -4,8 +4,8 @@
 #define SENSOR4 PC5
 #define SENSOR5 PA4
 #define SENSOR6 PA5
-#define SENSOR7 PA6
-#define SENSOR8 PA7
+// #define SENSOR7 PA6
+// #define SENSOR8 PA7
 
 HardwareSerial Serial2(PA10, PA9);
 
@@ -18,8 +18,8 @@ void setup() {
   pinMode(SENSOR4, INPUT);
   pinMode(SENSOR5, INPUT);
   pinMode(SENSOR6, INPUT);
-  pinMode(SENSOR7, INPUT);
-  pinMode(SENSOR8, INPUT);
+  // pinMode(SENSOR7, INPUT);
+  // pinMode(SENSOR8, INPUT);
 }
 
 void loop() {
@@ -62,10 +62,10 @@ void loop() {
       Serial2.print(analogRead(SENSOR5));
       Serial2.print("#");
       Serial2.print(analogRead(SENSOR6));
-      Serial2.print("#");
-      Serial2.print(analogRead(SENSOR7));
-      Serial2.print("#");
-      Serial2.print(analogRead(SENSOR8));
+      // Serial2.print("#");
+      // Serial2.print(analogRead(SENSOR7));
+      // Serial2.print("#");
+      // Serial2.print(analogRead(SENSOR8));
       Serial2.print("\n");
       delay(delay_ms);
     }
--- micro_controller/serial_data.py
+++ micro_controller/serial_data.py
@@ -1,15 +1,18 @@
 import serial
 import datetime
 import csv
+import multiprocessing
 
 ser = serial.Serial('COM7', baudrate=115200)  # usb port settings
-timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
-csv_filename = f"sensor_data_{timestamp}.csv"
 
 
 def request_data(delay, amount):
-    data_to_send = f"{delay}#{amount}\n"
-    ser.write(data_to_send.encode())
+    data_to_send = f"{delay}#{amount}\n"  # data format
+    ser.write(data_to_send.encode())  # send data to usb
+
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+    csv_filename = f"sensor_data_{timestamp}.csv"
+
     with open(csv_filename, mode='w', newline='') as csv_file:
         for i in range(int(amount)):
             serial_data = ser.readline().decode('ascii')    # read serial data from usb
@@ -20,8 +23,8 @@ def request_data(delay, amount):
             csv_writer.writerow(int_values)  # append data to csv file
 
 
-while 1:
+if __name__ == "__main__":
     delay = input("Delay (ms): ")  # input delay from user
     amount = input("How many data? ")  # input amount from user
-    # print(delay+amount)
-    request_data(delay, amount)
+    p1 = multiprocessing.Process(target=request_data, args=(delay, amount))
+    p1.start()
