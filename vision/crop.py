1776cee2862f2f27ae696732c01d7f6621d307b2
PengPengPoka <hafizhfanani03@gmail.com>
2023-09-29 09:08:57 +0700

    nambah file crop

---
+++ vision/crop.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import numpy as np
+import os
+
+def main():
+    home = os.path.expanduser("~")
+    img_path = home + "\\OneDrive\\Pictures\\INSTEAD Teh"
+    
+
+
+if __name__ == "__main__":
+    main()
