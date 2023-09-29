210a868e0c01b50f02c30fe87318ff2ea859bf8a
Satria Pamungkas <satriap2101@gmail.com>
2023-09-29 09:08:27 +0700

    auto com port done

--- micro_controller/coba_auto_com.py
+++ micro_controller/coba_auto_com.py
@@ -6,14 +6,25 @@ import serial.tools.list_ports
 
 print('Search...')
 ports = list(serial.tools.list_ports.comports())
-for port in ports :
-    print('Find port '+ port.device)
+for port in ports:
+    print('Found port ' + port.device, port.serial_number)
 
 ser = serial.Serial(port.device)
 if ser.isOpen():
     ser.close()
 
-ser = serial.Serial(port.device, 9600, timeout=1)
+ser = serial.Serial(port.device, 115200)
 ser.flushInput()
 ser.flushOutput()
+ser.flush()
 print('Connect ' + ser.name)
+delay = 60
+amount = 30
+data_to_send = f"{delay}#{amount}\n"  # data format
+ser.write(data_to_send.encode())  # send data to usb
+for i in range(amount):
+    serial_data = ser.readline().decode('ascii')    # read serial data from usb
+    split_values = serial_data.split("#")   # split values by #
+    int_values = [int(value) for value in split_values]
+    print(int_values)  # Output: [123, 456, 789]
+
--- micro_controller/serial_data.py
+++ micro_controller/serial_data.py
@@ -3,11 +3,20 @@ import datetime
 import csv
 import multiprocessing
 import serial.tools.list_ports
+import os
+import sys
+import time
 
 
 def request_data(delay, amount):
-    ser = serial.Serial('COM7', baudrate=115200)  # usb port settings
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        print('Found port ' + port.device, port.serial_number)
+
+    ser = serial.Serial(port.device, 115200)
     ser.flush()
+    print('Connect ' + ser.name)
+
     data_to_send = f"{delay}#{amount}\n"  # data format
     ser.write(data_to_send.encode())  # send data to usb
 
@@ -16,25 +25,26 @@ def request_data(delay, amount):
 
     with open(csv_filename, mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        header = ["Sensor 1", "Sensor 2", "Sensor 3", "Sensor 4", "Sensor 5", "Sensor 6"]
+        header = ["Sensor 1", "Sensor 2", "Sensor 3",
+                  "Sensor 4", "Sensor 5", "Sensor 6"]
         csv_writer.writerow(header)
         for i in range(int(amount)):
             serial_data = ser.readline().decode('ascii')    # read serial data from usb
             split_values = serial_data.split("#")   # split values by #
             int_values = [int(value) for value in split_values]
             # for i in range(6):
-                # int_values[i] = round((int_values[i]*3.3/65536), 3)
-            print(int_values)  # Output: [123, 456, 789]
+            # int_values[i] = round((int_values[i]*3.3/65536), 3)
+            print(i+1, int_values)  # Output: [123, 456, 789]
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(int_values)  # append data to csv file
 
+    ser.close()
+
 
 if __name__ == "__main__":
-    delay = input("Delay (ms): ")  # input delay from user
-    amount = input("How many data? ")  # input amount from user
+    # delay = input("Delay (ms): ")  # input delay from user
+    # amount = input("How many data? ")  # input amount from user
+    delay = 60
+    amount = 30
     p1 = multiprocessing.Process(target=request_data, args=(delay, amount))
     p1.start()
-    # print("STATUS: Mulai Ambil Data")
-    # p1.join()
-    # print("STATUS: Selesai Ambil Data")
-    # request_data(delay, amount)
