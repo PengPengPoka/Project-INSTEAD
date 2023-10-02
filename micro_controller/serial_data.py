3cc29cd98da811f98161fc44fcb3f983795bb816
Satria Pamungkas <satriap2101@gmail.com>
2023-10-02 12:59:58 +0700

    update auto com port

--- micro_controller/serial_data.py
+++ micro_controller/serial_data.py
@@ -9,48 +9,56 @@ import time
 
 
 def request_data(delay, amount):
-    print("Searching for COM ports...")
-    ports = list(serial.tools.list_ports.comports())
-    for port in ports:
-        print('Found port ' + port.device, port.serial_number)
-        ser = serial.Serial(port.device, 115200, timeout=1)
-        ser.flush()
-        print('Connect ' + ser.name)
+    for i in range(5):
+        print("Searching for COM ports...")
+        ports = list(serial.tools.list_ports.comports())
+        for port in ports:
+            try:
+                print('Found port ' + port.device, port.serial_number)
+                ser = serial.Serial(port.device, 115200, timeout=1)
+                ser.flush()
+                print('Connect ' + ser.name)
 
-        try:
-            data_to_test = f"{60}#{1}\n"  # data format
-            ser.write(data_to_test.encode())  # send data to usb
-            serial_data_test = ser.readline().decode('ascii')
-            if serial_data_test == '':
-                print("Wrong COM port")
-                raise Exception("Wrong COM port")
-        except:
-            print("error")
-            ser.close()
-            continue
+                data_to_test = f"{60}#{1}\n"  # data format
+                ser.write(data_to_test.encode())  # send data to usb
+                serial_data_test = ser.readline().decode('ascii')
+                if serial_data_test == '':
+                    print("Wrong COM port")
+                    raise Exception("Wrong COM port")
+                split_values_test = serial_data_test.split(
+                    "#")   # split values by #
+                int_values_test = [int(value) for value in split_values_test]
+                if int_values_test.__len__() < 6:
+                    print("Wrong COM port")
+                    raise Exception("Wrong COM port")
 
-        data_to_send = f"{delay}#{amount}\n"  # data format
-        ser.write(data_to_send.encode())  # send data to usb
+            except:
+                print("error")
+                ser.close()
+                continue
 
-        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
-        csv_filename = f"sensor_data_{timestamp}.csv"
+            data_to_send = f"{delay}#{amount}\n"  # data format
+            ser.write(data_to_send.encode())  # send data to usb
 
-        with open(csv_filename, mode='w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            header = ["Sensor 1", "Sensor 2", "Sensor 3",
-                      "Sensor 4", "Sensor 5", "Sensor 6"]
-            csv_writer.writerow(header)
-            for i in range(int(amount)):
-                serial_data = ser.readline().decode('ascii')    # read serial data from usb
-                split_values = serial_data.split("#")   # split values by #
-                int_values = [int(value) for value in split_values]
-                # for i in range(6):
-                # int_values[i] = round((int_values[i]*3.3/65536), 3)
-                print(i+1, int_values)  # Output: [123, 456, 789]
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+            csv_filename = f"sensor_data_{timestamp}_BOHEA_AMPAS_6-{i+1}.csv"
+
+            with open(csv_filename, mode='w', newline='') as csv_file:
                 csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(int_values)  # append data to csv file
-            ser.close()
-            break
+                header = ["Sensor 1", "Sensor 2", "Sensor 3",
+                          "Sensor 4", "Sensor 5", "Sensor 6"]
+                csv_writer.writerow(header)
+                for i in range(int(amount)):
+                    serial_data = ser.readline().decode('ascii')    # read serial data from usb
+                    split_values = serial_data.split("#")   # split values by #
+                    int_values = [int(value) for value in split_values]
+                    # for i in range(6):
+                    # int_values[i] = round((int_values[i]*3.3/65536), 3)
+                    print(i+1, int_values)  # Output: [123, 456, 789]
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow(int_values)  # append data to csv file
+                ser.close()
+                break
 
 
 if __name__ == "__main__":
